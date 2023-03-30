e0836965169a3da3feab440e68430306252afd26
Oksana Kozak <varewka7@gmail.com>
2023-03-30 12:55:19 +0300

    before check

---
+++ src/js/scroll.js
@@ -0,0 +1,25 @@
+SmoothScroll({
+  //  800 = 0.8 секунди
+  animationTime: 800,
+  // в пікселях
+  stepSize: 75,
+
+  // Прискорення
+  accelerationDelta: 30,
+  // Максимальне прискорення
+  accelerationMax: 2,
+
+  // Підтримка клавіатури
+  keyboardSupport: true,
+  // Крок скрола стрілками на клавіатурі
+  arrowScroll: 50,
+
+  // Pulse (less tweakable)
+  // ratio of "tail" to "acceleration"
+  pulseAlgorithm: true,
+  pulseScale: 4,
+  pulseNormalize: 1,
+
+  //Підтримка тачпада
+  touchpadSupport: true,
+});
